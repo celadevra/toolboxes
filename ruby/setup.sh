858336fe91c960e0959fde3d3900b4b1bd6a1644
Haoyang Xu <haoyang@expoundite.net>
2020-06-16 12:05:13 +0800

    rename toolbox

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+TBX_NAME=ruby
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-alias ruby='toolbox run -c getpocket ruby'
-alias gem='toolbox run -c getpocket gem'
-alias be='toolbox run -c getpocket bundle exec'
-alias bundle='toolbox run -c getpocket bundle'
+alias ruby='toolbox run -c TBX_NAME ruby'
+alias gem='toolbox run -c TBX_NAME gem'
+alias be='toolbox run -c TBX_NAME bundle exec'
+alias bundle='toolbox run -c TBX_NAME bundle'
 alias emacs='emacs -l $DIR/tools.el'
