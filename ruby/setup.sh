87562a7683ca9ab309e1bcc483463cc467707d07
Haoyang Xu <haoyang@expoundite.net>
2020-06-17 16:00:30 +0800

    irb commands

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 TBX_NAME=ruby
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-alias ruby='toolbox run -c TBX_NAME ruby'
-alias gem='toolbox run -c TBX_NAME gem'
-alias be='toolbox run -c TBX_NAME bundle exec'
-alias bundle='toolbox run -c TBX_NAME bundle'
+alias ruby='toolbox run -c $TBX_NAME ruby'
+alias gem='toolbox run -c $TBX_NAME gem'
+alias be='toolbox run -c $TBX_NAME bundle exec'
+alias bundle='toolbox run -c $TBX_NAME bundle'
+alias irb='toolbox run -c $TBX_NAME irb'
 alias emacs='emacs -l $DIR/tools.el'
